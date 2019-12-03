54c0ea4b3051997ec50c5144bf5e88aa2f76f66a
Andrew R. DeFilippis <andrewdefilippis@gmail.com>
2019-12-02 17:08:56 -0800

    output to json and yaml files + backfill from previous years rtp data

--- rtp_web_scraper.py
+++ rtp_web_scraper.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import yaml
 from datetime import datetime
 from re import match
 from urllib.request import urlopen
@@ -10,7 +11,7 @@ from bs4 import BeautifulSoup
 
 
 def html_parser(data, year):
-    json_file = {year: {}}
+    json_output = {year: {}}
     text = ""
 
     soup = BeautifulSoup(data, "html.parser")
@@ -21,12 +22,12 @@ def html_parser(data, year):
             try:
                 if match('^[A-Za-z]', td.get_text()):
                     text = td.get_text()
-                    json_file[year][text] = None
+                    json_output[year][text] = None
                 elif match('^[0-9]', td.get_text()):
-                    json_file[year][text] = td.get_text()
+                    json_output[year][text] = td.get_text()
             except Exception as e:
                 continue
-    return json_file
+    return json_output
 
 
 def main():
@@ -49,14 +50,22 @@ def main():
             raise ValueError
 
         data = urlopen(url).read()
-        json_file = html_parser(data, year)
+        json_output = html_parser(data, year)
 
     except ValueError as e:
         raise ValueError("Value supplied for year is invalid.")
     except Exception as e:
         raise Exception(e)
 
-    print(json.dumps(json_file, indent=2, sort_keys=True))
+    formatted_json = json.dumps(json_output, indent=2, sort_keys=True)
+    with open("./JSON/{}.json".format(year), "w") as json_file:
+        json_file.write(formatted_json)
+    with open("./YAML/{}.yaml".format(year), "w") as yaml_file:
+        yaml_file.write("---\n")
+        yaml_file.write(yaml.dump(json_output))
+
+    print(formatted_json)
+    print("\n---\n" + yaml.dump(json_output))
 
 
 if __name__ == '__main__':
