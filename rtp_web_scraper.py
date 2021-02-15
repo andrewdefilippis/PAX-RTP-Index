5450632856cba4391c231d878af309e800e911e6
Andrew R. DeFilippis <andrew@defilippisfamily.com>
2021-02-15 00:24:55 -0800

    Strip whitespace, fix missing current-year rtp page, support current and next year

--- rtp_web_scraper.py
+++ rtp_web_scraper.py
@@ -11,22 +11,38 @@ from bs4 import BeautifulSoup
 
 
 def html_parser(data, year):
-    json_output = {year: {}}
     text = ""
 
     soup = BeautifulSoup(data, "html.parser")
+    h1_tags = soup.find_all("h1")
     table = soup.find("table")
 
+    # Confirm that the page being viewed is the same year being requested
+    for header in h1_tags:
+        h_text = header.get_text()
+        if match('^[0-9]{4} PAX/RTP Index', h_text):
+            h_year = h_text.split(' ')[0]
+
+            if year is not None and not match("^{}".format(year), h_text):
+                raise ValueError("The year \"{}\" in the header on the PAX page, does not match the requested year: {}".format(h_year, year))
+            elif year is None:
+                year = h_year
+                json_output = {year: {}}
+            else:
+                json_output = {year: {}}
+
+    # Loop through the rows and columns for the PAX/RTP data
     for row in table.find_all("tr"):
         for td in row.find_all("td"):
             try:
                 if match('^[A-Za-z]', td.get_text()):
-                    text = td.get_text()
+                    text = td.get_text().strip()
                     json_output[year][text] = None
                 elif match('^[0-9]', td.get_text()):
-                    json_output[year][text] = td.get_text()
+                    json_output[year][text] = td.get_text().strip()
             except Exception as e:
                 continue
+
     return json_output
 
 
@@ -35,27 +51,34 @@ def main():
     parser.add_argument("-y", "--year", type=int, help="Year for PAX/RTP data")
     args = parser.parse_args()
     year = args.year
+    now_year = datetime.now().year
+
     earliest_rtp_year = 1995
+    base_page = "http://solotime.info/pax/"
+    rtp_html = "rtp{}.html"
 
-    try:
-        try:
-            if year is None:
-                year = datetime.now().year
-                url = "http://solotime.info/pax/rtp{}.html".format(year)
-            elif earliest_rtp_year <= year:
-                url = "http://solotime.info/pax/rtp{}.html".format(year)
-            else:
-                raise
-        except Exception as e:
-            raise ValueError
+    # From the /rtp2021.html document as of 2021-02-15:
+    #
+    # Please update your links and bookmarks to http://solotime.info/pax/
+    # This corrected link will then be correct every year and always have the newest numbers!
+    if year is None or year == now_year:
+        year = None
+        url = base_page
+    elif year == now_year + 1:
+        # Allow specification of the next year for when RTP data is released before January 1st of said year.
+        # The year will be validated against the year specified in the header on the PAX html page.
+        url = base_page
+    elif year >= earliest_rtp_year and year <= now_year - 1:
+        url = base_page + rtp_html.format(year)
+    else:
+        raise ValueError("Value supplied for year must be from {} to {}.".format(earliest_rtp_year, now_year))
 
-        data = urlopen(url).read()
-        json_output = html_parser(data, year)
+    data = urlopen(url).read()
 
-    except ValueError as e:
-        raise ValueError("Value supplied for year is invalid.")
-    except Exception as e:
-        raise Exception(e)
+    if data is None:
+        raise ValueError("RTP page for {} year was not found".format(year))
+
+    json_output = html_parser(data, year)
 
     formatted_json = json.dumps(json_output, indent=2, sort_keys=True)
     with open("./JSON/{}.json".format(year), "w") as json_file:
