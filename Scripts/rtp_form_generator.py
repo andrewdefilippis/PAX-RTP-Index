546ebb98ad8a2add150cf3ff36f083b0e5a923de
🚀 Andrew R. DeFilippis <andrew@defilippisfamily.com>
2023-12-10 16:58:57 -0800

    RTP Form cleanup

--- Scripts/rtp_form_generator.py
+++ Scripts/rtp_form_generator.py
@@ -19,25 +19,14 @@ def main():
     if year is None:
         year = datetime.now().year
 
-    with open("./JSON/{}.json".format(year), "r") as json_file:
+    with open("./rtp.json", "r") as json_file:
         pax_rtp_data = json.load(json_file)
         print(pax_rtp_data)
-        print(pax_rtp_data[str(year)]["STS"])
+        #print(pax_rtp_data[str(year)]["STS"])
 
-        group1 = ["STS", "STX", "STR", "STU", "STH"]
-        group2 = ["XS-A", "XS-B", "EV"]
-        group1dict = {}
-        group2dict = {}
-        groups = []
-        groupsdict = {}
-        for group in group1:
-            group1dict[group] = pax_rtp_data[str(year)][group]
-        for group in group2:
-            group2dict[group] = pax_rtp_data[str(year)][group]
-        groups.append(group1dict)
-        groups.append(group2dict)
-        groupsdict['group'] = groups
-        print(groups)
+        # for each class, get current year's rtp and category
+        # filter out classes without 2021 rtp
+        # group together in the table
 
         table = json2table.convert(groupsdict,
                                    build_direction=build_direction,
@@ -49,17 +38,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# Categories:
-# Street ([A-Z]S|HCS) - Heritage Classic
-# Street Touring (ST[A-Z])
-# Street Prepared ([A-Z]SP)
-# Street Modified ([A-Z]?SM[A-Z]?)
-# Prepared ([A-Z]P|HCR) - Heritage Classic
-# Modified ([A-Z]M)
-# FSAE (FSAE)
-# Classic American Muscle (CAM-[A-Z])
-# Solo Spec Coupe (SSC)
-# Street-R (SSR) - Part of Street Prepared but separate
-# Kart Modified (KM|J[A-Z]) - Formula Junior
-# Xtreme Street (XS-[A-Z]|EV)
